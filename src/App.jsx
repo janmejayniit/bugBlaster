2d8c9de4558ca83342db54b55650a31b1c5fdc12
janmejayniit <janmejayniit@gmail.com>
2025-12-24 13:12:15 +0530

    Add reducer, sorting, edit and delete in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { sortTickets } from './utilities/sortingUtilities.jsx'
 import './styles.css'
+import './App.css'
 import TicketForm from './components/TicketForm.jsx'
+import ticketReducer from './reducers/ticketReducer.jsx'
+import { useReducer } from 'react'
+import TicketList from './components/TicketList.jsx'
 
 function App() {
-  
-
+  const initialState = {tickets:[], editingTicket:null, sortPreference:"High to Low"}
+  const [state, dispatch] = useReducer(ticketReducer, initialState)
+   const sortedTickets = sortTickets(state.tickets, state.sortPreference);
   return (
     <>
       <div className="container">
         <h1>Bug Blaster</h1>
-        <TicketForm />
+        <TicketForm dispatch={dispatch} editingTicket={state.editingTicket} />
+        {state.tickets.length>0 && (
+          <>
+            <h2>All Tickets</h2>
+
+            <select value={state.sortPreference} onChange={(e) => dispatch({type:"SET_SORTING", payload:e.target.value})}>
+              <option value="High to Low">High to Low</option>
+              <option value="Low to High">Low to High</option>
+            </select>
+            <TicketList tickets={sortedTickets} dispatch={dispatch} />
+          </>   
+        )}
+       
       </div>
         
     </>
--- src/components/TicketForm.jsx
+++ src/components/TicketForm.jsx
@@ -1,10 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
-export default function TicketForm(){
+export default function TicketForm({dispatch, editingTicket}){
 
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
-    const [priority, setPriority] = useState('Low');
+    const [priority, setPriority] = useState("1");
+
+    useEffect(() => {
+        if(editingTicket){
+            setTitle(editingTicket.title);
+            setDescription(editingTicket.description);
+            setPriority(editingTicket.priority.toString());
+        }
+    },[editingTicket]);
 
     const priorityLabels = {
         1: 'Low',
@@ -21,25 +29,31 @@ export default function TicketForm(){
     const handleSubmit = (e) =>{
         e.preventDefault();
         const ticketData = {
-            id: new Date().toISOString(),
+            id: editingTicket?editingTicket.id:new Date().toISOString(),
             title,
             description,
             priority
         };
         console.log('Ticket Submitted:', ticketData);
+        dispatch({type: editingTicket? "UPDATE_TICKET": "ADD_TICKET", payload: ticketData});
         clearForm();
     }
+
+    const handleCancel = () => {
+        dispatch({ type: "CLEAR_EDITING_TICKET" });
+        clearForm();
+    };
 
     return(
         <>
             <form onSubmit={handleSubmit} className="ticket-form">
                 <div>
                     <label>Title</label>
-                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />  
+                    <input type="text" className="form-input" value={title} onChange={(e) => setTitle(e.target.value)} />  
                 </div>
                 <div>
                     <label>Description</label>
-                    <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
+                    <textarea className="form-input" value={description} onChange={(e) => setDescription(e.target.value)} />
                 </div>
                 <fieldset className='priority-fieldset'>
                     <legend>Priority</legend>
@@ -49,6 +63,7 @@ export default function TicketForm(){
                             <input
                                 type="radio"
                                 id={`priority-${key}`}
+                                className="priority-input"
                                 name="priority"
                                 value={key}
                                 checked={priority === key}
@@ -60,6 +75,11 @@ export default function TicketForm(){
                     ))}
                 </fieldset>                
                 <button type="submit" className="button">Submit Ticket</button>
+                 {editingTicket && (
+                        <button className="button" onClick={handleCancel}>
+                        Cancel Edit
+                        </button>
+                    )}
             </form>
         </>
     )
